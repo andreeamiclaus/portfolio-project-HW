338ac02274326f6eb56a900a77fde32f61169152
andreeamiclaus <amiclaus.a@gmail.com>
2022-09-10 15:39:03 +0200

    Update

---
+++ home.js
@@ -0,0 +1,9 @@
+function openWindow1(){
+    window.open("https://ephemeral-lily-e0bc3d.netlify.app/");
+}
+function openWindow2(){
+    window.open("https://laughing-varahamihira-4175e3.netlify.app/");
+}
+function openWindow3(){
+    window.open("https://effulgent-bublanina-b4b0a2.netlify.app/");
+}
